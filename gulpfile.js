b074ce043a8b87e5515108189ce1352fd87c8d86
Evin Callahan <evin.callahan@gmail.com>
2019-02-16 12:38:01 -0600

    fix gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,23 @@ const gulp  = require('gulp');
 const gls   = require('gulp-live-server');
 const del   = require('del');
 
+const localConfig = './app/config/config/local.yml';
+
 function clean() {
-  return del([
-    './app/config/config/local.yml'
-  ])
+  return del([localConfig]);
 }
 
 function serve() {
   const server = gls('app/app.js', {env: process.env});
+  const watcher = gulp.watch(['app/**/*', `!${localConfig}`]);
   server.start();
 
-  return gulp.watch('app/**/*', function (file) {
+  watcher.on('all', function(event, path, stats) {
+    console.log('File ' + path + ' got event ' + event + ', restarting ...');
     server.start.bind(server)();
   });
+
+  return watcher;
 }
 
 exports.clean = clean;
